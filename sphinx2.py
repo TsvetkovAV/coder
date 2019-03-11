28893bfb28081014051f8154b9605683447f8026
TsvetkovAV <tsvetkov93dr@yandex.ru>
2019-03-11 19:16:57 +0800

    wrapped spinx2 with warning catch

--- sphinx2.py
+++ sphinx2.py
@@ -4,6 +4,7 @@ import re
 from src.utils import PATH, validate_row
 from difflib import get_close_matches
 from src.normalize import normalize_desc
+import warnings
 
 db = pymysql.connect(host='127.0.0.1',
                              port=9306,
@@ -37,57 +38,59 @@ csv_data = csv.reader(data)
 #           'sum((word_count+(lcs-1)*max_lcs)*user_weight)', 'field_mask',
 #           'sum((4*lcs+2*(min_hit_pos==1)+exact_hit)*user_weight)*1000+bm25']:
 # for i in [0.2, 0.4, 0.6, 0.8]:
-for i in [0.2]:
-    data.seek(0)
-    fails = 0
-    wins = 0
-    for row in csv_data:
-        if validate_row(row):
-            train_desc = row[0]
-            train_manuf = row[1]
-            correct_code = row[2]
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    for i in [0.2]:
+        data.seek(0)
+        fails = 0
+        wins = 0
+        for row in csv_data:
+            if validate_row(row):
+                train_desc = row[0]
+                train_manuf = row[1]
+                correct_code = row[2]
 
-            train_manuf = re.sub('[.,()+"\']+', '', train_manuf).strip()
+                train_manuf = re.sub('[.,()+"\']+', '', train_manuf).strip()
 
-            # Bring to Puls format
-            # N90 -> х90
-            train_desc = re.sub('N(\d+)', r'х\1', train_desc)
-            # 2.0 -> 2
-            train_desc = re.sub('(\d+)[,.]0([\D$])', r'\1\2', train_desc)
-            # 0.005 -> 5 мг
-            train_desc = re.sub('(\d+)([,.])(\d+)', Mg_to_mg, train_desc)
+                # Bring to Puls format
+                # N90 -> х90
+                train_desc = re.sub('N(\d+)', r'х\1', train_desc)
+                # 2.0 -> 2
+                train_desc = re.sub('(\d+)[,.]0([\D$])', r'\1\2', train_desc)
+                # 0.005 -> 5 мг
+                train_desc = re.sub('(\d+)([,.])(\d+)', Mg_to_mg, train_desc)
 
-            train_desc, desc_form, amount, dosage = normalize_desc(train_desc)
+                train_desc, desc_form, amount, dosage = normalize_desc(train_desc)
 
-            form_query = desc_form.split(' ')
-            if len(form_query) > 1:
-                form_query = '"'+' | '.join(form_query)+'"'
-            else:
-                form_query = form_query[0]
+                form_query = desc_form.split(' ')
+                if len(form_query) > 1:
+                    form_query = '"'+' | '.join(form_query)+'"'
+                else:
+                    form_query = form_query[0]
 
-            train_desc = re.sub('[^\da-zа-я,.\s%]', ' ', train_desc)
+                train_desc = re.sub('[^\da-zа-я,.\s%]', ' ', train_desc)
 
-            try:
-                query = "Select *, WEIGHT() from puls_index "\
-                            "where amount = "+amount+" and MATCH('"\
-                            "@form \""+desc_form+"\"/0.2 "\
-                            "@dosage \""+dosage+"\"/0.2 "\
-                            "@description \""+train_desc+"\"/0.2 ') "\
-                        "limit 5 OPTION ranker=expr('bm25')"
-                # print(query)
-                cursor.execute(query)
-            except Exception as exc:
-                print(exc)
-            result = cursor.fetchall()
+                try:
+                    query = "Select *, WEIGHT() from puls_index "\
+                                "where amount = "+amount+" and MATCH('"\
+                                "@form \""+desc_form+"\"/0.2 "\
+                                "@dosage \""+dosage+"\"/0.2 "\
+                                "@description \""+train_desc+"\"/0.2 ') "\
+                            "limit 5 OPTION ranker=expr('bm25')"
+                    # print(query)
+                    cursor.execute(query)
+                except Exception as exc:
+                    print(exc)
+                result = cursor.fetchall()
 
-            if len(result) == 0:
-                fails += 1
-                continue
-            if int(correct_code) in [res['code'] for res in result]:
-                if result[0]['code'] == int(correct_code):
-                    wins += 1
-            else:
-                # print(row[0], '-----', train_desc, '-----', row[3])
-                fails += 1
-    print(fails, 'fails('+str(round(fails/10138, 3))+') with i =', str(i))
-    print('Wins -', wins, '('+str(round(wins/10138, 3))+')')
+                if len(result) == 0:
+                    fails += 1
+                    continue
+                if int(correct_code) in [res['code'] for res in result]:
+                    if result[0]['code'] == int(correct_code):
+                        wins += 1
+                else:
+                    # print(row[0], '-----', train_desc, '-----', row[3])
+                    fails += 1
+        print(fails, 'fails('+str(round(fails/10138, 3))+') with i =', str(i))
+        print('Wins -', wins, '('+str(round(wins/10138, 3))+')')
